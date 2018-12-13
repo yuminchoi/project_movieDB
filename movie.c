e255f0c9ec591d2b8a284dea7577d0027d35ea98
yuminchoi <chldbals219@naver.com>
2018-12-13 14:50:49 +0900

    movie.c 변경
    실행불가

--- movie.c
+++ movie.c
@@ -16,7 +16,12 @@ void* mv_genMvInfo(char* name, float score, int runTime, char* country)
 	movInfo_t* mvPtr;
 	
 	//allocate memory and set the member variables
-	mvPtr=(movInfo_t*)malloc(size of(movInfo_t));		//동적메모리 할당 
+	mvPtr=(movInfo_t*)malloc(sizeof(movInfo_t));		//동적메모리 할당
+	
+	mvPtr->name=name;		//변수설정 
+	mvPtr->score=score;
+	mvPtr->runTime=runTime;
+	mvPtr->madeIn=country; 
 	
 	return (void*)mvPtr;
 }
@@ -40,25 +45,37 @@ void mv_print(void* obj)
 //return the score value from the input instance of movInfo_t structure
 float mv_getScore(void* obj)
 {
+	movInfo_t* mvPtr=(movInfo_t*)obj;
 	
+	mvPtr->;
+	return obj;	
 }
 
 //return the runtime value from the input instance of movInfo_t structure
 int mv_getRunTime(void* obj)
 {
+	movInfo_t* mvPtr;
 	
+	mvPtr->runTime=()obj;
+	return mvPtr;
 }
 
 //return the name string pointer from the input instance of movInfo_t structure
 char* mv_getName(void* obj)
 {
+	movInfo_t* mvPtr;
 	
+	mvPtr->name=obj;
+	return mvPtr;
 }
 
 //return the country string pointer from the input instance of movInfo_t structure
 char* mv_getCountry(void* obj)
 {
+	movInfo_t* mvPtr;
 	
+	mvPtr->madeIn=obj;
+	return mvPtr;
 }
 
 
